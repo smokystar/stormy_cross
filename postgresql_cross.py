cf87036d56e5aacab2fcef37aa8af91e89bcccc1
smokystar <101525606+smokystar@users.noreply.github.com>
2022-11-06 19:58:04 +0300

    credentials

--- postgresql_cross.py
+++ postgresql_cross.py
@@ -1,7 +1,7 @@
 import psycopg2
-con = psycopg2.connect(user="postgres",
-                                password="5466",
-                                host="localhost",
+con = psycopg2.connect(user="udzclombmplopn",
+                                password="006bbc0a282a6a60d71ab63eceb99327b4c73794c97b3e968d74de449d939e4d",
+                                host="ec2-54-155-129-189.eu-west-1.compute.amazonaws.com",
                                 port="5432",
-                                database="cross_exch")
+                                database="d9tf4mansnquj2")
 cur = con.cursor()
