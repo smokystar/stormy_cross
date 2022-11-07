0b1c7d200a9f44e1d942923456c25e95c0334eaf
smokystar <101525606+smokystar@users.noreply.github.com>
2022-11-07 12:28:18 +0300

    Update main_cross.py

--- main_cross.py
+++ main_cross.py
@@ -10,24 +10,9 @@ import commands
 # cur.execute(f'CREATE TABLE tickers(id bigint primary key, coin_name text,binance text, bybit text, ftx text, huobi text, kucoin text, mexc text, phemex text, date_time text)')
 # con.commit()
 
-cur.execute(f'delete from tickers')
-con.commit()
-cur.execute(f'alter table tickers add column aax text')
-con.commit()
-cur.execute(f'alter table tickers add column bitget text')
-con.commit()
-cur.execute(f'alter table tickers add column poloniex text')
-con.commit()
-cur.execute(f'alter table tickers add column bitforex text')
-con.commit()
-cur.execute(f'alter table tickers add column bitfinex text')
-con.commit()
-cur.execute(f'alter table tickers add column upbit text')
-con.commit()
-cur.execute(f'alter table tickers add column gateio text')
-con.commit()
-cur.execute(f'alter table tickers add column bithumb text')
-con.commit()
+#cur.execute(f'delete from tickers')
+#con.commit()
+
 
 
 commands.register_handlers_client_partners(dp)
