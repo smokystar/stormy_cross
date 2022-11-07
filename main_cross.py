0799e22b90a80cb3cba2d62a3993d8b67eb6614c
smokystar <101525606+smokystar@users.noreply.github.com>
2022-11-07 12:10:38 +0300

    Update main_cross.py

--- main_cross.py
+++ main_cross.py
@@ -6,9 +6,30 @@ async def on_startup(_):
     print('Bot is online')
 
 import commands
-cur.execute(f'CREATE TABLE tickers(id bigint primary key, coin_name text, '
+
+# cur.execute(f'CREATE TABLE tickers(id bigint primary key, coin_name text, '
             f'binance text, bybit text, ftx text, huobi text, kucoin text, mexc text, phemex text, date_time text)')
+# con.commit()
+
+cur.execute(f'delete from tickers')
 con.commit()
+cur.execute(f'alter table tickers add column aax text')
+con.commit()
+cur.execute(f'alter table tickers add column bitget text')
+con.commit()
+cur.execute(f'alter table tickers add column poloniex text')
+con.commit()
+cur.execute(f'alter table tickers add column bitforex text')
+con.commit()
+cur.execute(f'alter table tickers add column bitfinex text')
+con.commit()
+cur.execute(f'alter table tickers add column upbit text')
+con.commit()
+cur.execute(f'alter table tickers add column gateio text')
+con.commit()
+cur.execute(f'alter table tickers add column bithumb text')
+con.commit()
+
 
 commands.register_handlers_client_partners(dp)
 
